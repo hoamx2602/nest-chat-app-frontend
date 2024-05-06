85a14e4baa0274a244e3664440ade85c7c075fe2
hoamx <hoa.mx@kiotviet.com>
2024-05-06 07:24:53 +0700

    feat: add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const App = () => {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark Mode</h1>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default App;
