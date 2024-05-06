c55f97f6c9998ee7eb0eab8bb908b5970187ed2f
hoamx <hoa.mx@kiotviet.com>
2024-05-06 15:12:45 +0700

    feat: handle mutation createUser with UI and cors

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 const Signup = () => {
+  const [createUser] = useCreateUser();
+
+  const handleSubmit = async (email: string, password: string) => {
+    await createUser({
+      variables: {
+        createUserInput: {
+          email,
+          password,
+        },
+      },
+    });
+  };
+
   return (
-    <Auth submitLabel="Signup" onSubmit={async () => {}}>
+    <Auth submitLabel="Signup" onSubmit={({ email, password }) => handleSubmit(email, password)}>
       <Link to={"/login"} style={{ alignSelf: "center" }}>
         <MUILink>Login</MUILink>
       </Link>
