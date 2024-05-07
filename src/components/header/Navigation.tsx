a7dff8fac86e3ca3f727c2889cdd74c0f6dc9982
hoamx <hoa.mx@kiotviet.com>
2024-05-07 08:33:12 +0700

    feat: handle authenticated state

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,16 +1,22 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { Page } from "../../interfaces";
+import router from "../Routes";
 
 interface NavigationProps {
-  pages: string[];
+  pages: Page[];
 }
 
 const Navigation = ({ pages }: NavigationProps) => {
   return (
     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
       {pages.map((page) => (
-        <Button key={page} sx={{ my: 2, color: "white", display: "block" }}>
-          {page}
+        <Button
+          key={page.title}
+          sx={{ my: 2, color: "white", display: "block" }}
+          onClick={() => router.navigate(page.path)}
+        >
+          {page.title}
         </Button>
       ))}
     </Box>
--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,7 +1,9 @@
 import router from "../components/Routes";
 import client from "../constants/apollo-client";
+import { authenticatedVar } from "../constants/authenticated";
 
 export const onLogout = () => {
+  authenticatedVar(false);
   client.resetStore();
   router.navigate("/login");
 };
