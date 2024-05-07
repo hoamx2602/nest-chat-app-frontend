71d4449f15f01d805a3825207505e1464375e34b
hoamx <hoa.mx@kiotviet.com>
2024-05-07 12:39:22 +0700

    feat: handle create chat component

--- src/components/chat-list/ChatList.tsx
+++ src/components/chat-list/ChatList.tsx
@@ -3,46 +3,55 @@ import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ChatListHeader from "./chat-list-header/ChatListHeader";
 import ChatListItem from "./chat-list-item/ChatListItem";
+import { useState } from "react";
+import ChatListAdd from "./chat-list-add/ChatListAdd";
 
 const ChatList = () => {
+  const [chatListAddVisible, setAddChatListVisible] = useState(false);
   return (
-    <Stack>
-      <ChatListHeader />
-      <Divider/>
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "background.paper",
-          maxHeight: "80vh",
-          overflow: "auto",
-        }}
-      >
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-      </List>
-    </Stack>
+    <>
+      <ChatListAdd
+        open={chatListAddVisible}
+        handleClose={() => setAddChatListVisible(false)}
+      />
+      <Stack>
+        <ChatListHeader handleAddChat={() => setAddChatListVisible(true)} />
+        <Divider />
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            maxHeight: "80vh",
+            overflow: "auto",
+          }}
+        >
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+        </List>
+      </Stack>
+    </>
   );
 };
 
--- src/components/chat-list/chat-list-header/ChatListHeader.tsx
+++ src/components/chat-list/chat-list-header/ChatListHeader.tsx
@@ -1,16 +1,20 @@
 import { AddCircle } from "@mui/icons-material";
 import { AppBar, IconButton, Toolbar } from "@mui/material";
 
-const ChatListHeader = () => {
-  return ( 
+interface ChatListHeaderProps {
+  handleAddChat: () => void;
+}
+
+const ChatListHeader = ({ handleAddChat }: ChatListHeaderProps) => {
+  return (
     <AppBar position="static" color="transparent">
       <Toolbar>
-        <IconButton size="large" edge="start">
-          <AddCircle/>
+        <IconButton size="large" edge="start" onClick={handleAddChat}>
+          <AddCircle />
         </IconButton>
       </Toolbar>
     </AppBar>
-   );
-}
- 
+  );
+};
+
 export default ChatListHeader;
