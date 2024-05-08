4f9ab887524aa8f29be3136da51bb1244906cf39
hoamx <hoamx.work@gmail.com>
2024-05-08 23:15:40 +0700

    fix: change schema

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -34,6 +34,7 @@ const Signup = () => {
             variables: {
               createUserInput: {
                 email,
+                username,
                 password,
               },
             },
