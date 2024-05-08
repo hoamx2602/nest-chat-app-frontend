a19f2e282b16787ad58be0861e01dddf64cdd0ee
hoamx <hoa.mx@kiotviet.com>
2024-05-08 09:38:08 +0700

    feat: create ws link

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,10 +1,13 @@
-import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, from, split } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import { API_URL } from "./url";
+import { API_URL, WS_URL } from "./url";
 import excludedRoutes from "./excluded-routes";
 import { onLogout } from "../utils/logout";
 import { snackVar } from "./snack";
 import { UNKNOW_ERROR_SNACK_MESSAGE } from "./error";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const logoutLink = onError(({ graphQLErrors, networkError }) => {
   if (
@@ -17,15 +20,38 @@ const logoutLink = onError(({ graphQLErrors, networkError }) => {
   }
 
   if (networkError) {
-    snackVar(UNKNOW_ERROR_SNACK_MESSAGE)
+    snackVar(UNKNOW_ERROR_SNACK_MESSAGE);
   }
 });
 
 const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `ws://${WS_URL}/graphql`,
+  })
+);
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+// * refer: https://www.apollographql.com/docs/react/data/subscriptions/#3-split-communication-by-operation-recommended
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: from([logoutLink, httpLink]),
+  link: splitLink,
 });
 
 export default client;
